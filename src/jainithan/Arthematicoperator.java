dcf6f4cd2538581440dd22803486e1131fac1cce
jainithan007 <jainithan446@gmail.com>
2024-01-09 15:20:03 +0530

    first commit

---
+++ src/jainithan/Arthematicoperator.java
@@ -0,0 +1,20 @@
+package jainithan;
+
+public class Arthematicoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int  a = 11, b = 2 , c;
+	
+		
+		a  = a + b ;
+		
+		c = a++;
+		
+		System.out.println("a is " + c );
+		System.out.println("c is " + a );
+		
+
+	}
+
+} 
